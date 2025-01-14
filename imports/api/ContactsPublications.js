0c98a653d8e983ce865bf8cec67a406c39030723
Fabian Velasquez <fabian.velasquez@MacBook-Air.local>
2025-01-14 14:51:06 -0500

    Leccion 1.3.6

--- imports/api/ContactsPublications.js
+++ imports/api/ContactsPublications.js
@@ -1,6 +1,9 @@
 import { ContactsCollection } from './ContactsCollection.js';
 import { Meteor } from 'meteor/meteor';
 
-Meteor.publish('contacts', function publishContacts() {
+Meteor.publish('allContacts', function publishContacts() {
   return ContactsCollection.find();
+});
+Meteor.publish('contacts', function publishContacts() {
+  return ContactsCollection.find({ archived: { $ne: true }});
 });

1c0b7b7433476b13b1761032bb87d2476df269f1
Fabian Velasquez <fabian.velasquez@MacBook-Air.local>
2025-01-15 07:30:52 -0500

    Commitenado para volver a version anterior y corregir errores con simple schema

--- imports/api/ContactsPublications.js
+++
@@ -1,9 +0,0 @@
-import { ContactsCollection } from './ContactsCollection.js';
-import { Meteor } from 'meteor/meteor';
-
-Meteor.publish('allContacts', function publishContacts() {
-  return ContactsCollection.find();
-});
-Meteor.publish('contacts', function publishContacts() {
-  return ContactsCollection.find({ archived: { $ne: true }});
-});

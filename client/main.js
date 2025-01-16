f62370ea6b27d12c9405151bc5f16ac5c7d2482d
Fabian Velasquez <fabian.velasquez@okorum.com>
2025-01-15 19:40:44 -0500

    Leccion 3.4

--- client/main.js
+++ client/main.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { App } from '../ui/App';
 import '../api/methods/ContactsMethods';
 import '../api/methods/TransactionsMethods';
 
 Meteor.startup(() => {
-  render(<App />, document.getElementById('react-target'));
+  const root = createRoot(document.getElementById('react-target'));
+  root.render(<App />);
 });

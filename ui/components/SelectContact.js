efe06c2ed3326a51ce5e7ee24f0fbce2a98a0641
Fabian Velasquez <fabian.velasquez@okorum.com>
2025-01-15 20:00:03 -0500

    Leccion 3.5

--- ui/components/SelectContact.js
+++ ui/components/SelectContact.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 import { Listbox, Transition } from '@headlessui/react';
-import { CheckIcon, SelectorIcon } from '@heroicons/react/solid';
+import CheckIcon from '@heroicons/react/24/solid/CheckIcon';
+import ChevronUpDownIcon from '@heroicons/react/24/solid/ChevronUpDownIcon';
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
@@ -28,7 +29,7 @@ export const SelectContact = ({ title, contact, setContact, contacts }) => (
               </span>
             </span>
             <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
-              <SelectorIcon
+              <ChevronUpDownIcon
                 className="h-5 w-5 text-gray-400"
                 aria-hidden="true"
               />
